a5873acc038b6b6f88231c27f68b4c8a6221f15e
espr14 <espr14@gmail.com>
2021-01-25 21:49:05 +0100

    works

--- src/common/crash_recovery.cpp
+++ src/common/crash_recovery.cpp
@@ -11,98 +11,33 @@
 
 void do_homing_move_crash(const AxisEnum axis, const float distance, const feedRate_t fr_mm_s = 0.0) {
 
-#if HOMING_Z_WITH_PROBE && HAS_HEATED_BED && ENABLED(WAIT_FOR_BED_HEATER)
-    // Wait for bed to heat back up between probing points
-    if (axis == Z_AXIS && distance < 0 && thermalManager.isHeatingBed()) {
-        serialprintPGM(msg_wait_for_bed_heating);
-        LCD_MESSAGEPGM(MSG_BED_HEATING);
-        thermalManager.wait_for_bed();
-        ui.reset_status();
-    }
-#endif
-
     // Only do some things when moving towards an endstop
-    const int8_t axis_home_dir =
-#if ENABLED(DUAL_X_CARRIAGE)
-        (axis == X_AXIS) ? x_home_dir(active_extruder) :
-#endif
-                         home_dir(axis);
+    const int8_t axis_home_dir = home_dir(axis);
     const bool is_home_dir = (axis_home_dir > 0) == (distance > 0);
-
-#if ENABLED(SENSORLESS_HOMING)
-    sensorless_t stealth_states;
-#endif
-
-    if (is_home_dir) {
-
-#if HOMING_Z_WITH_PROBE && QUIET_PROBING
-        if (axis == Z_AXIS)
-            probing_pause(true);
-#endif
-
-// Disable stealthChop if used. Enable diag1 pin on driver.
-#if ENABLED(SENSORLESS_HOMING)
-        stealth_states = start_sensorless_homing_per_axis(axis);
-#endif
-    }
-
     const feedRate_t real_fr_mm_s = fr_mm_s ?: homing_feedrate(axis);
 
-#if ENABLED(MOVE_BACK_BEFORE_HOMING)
     if ((axis == X_AXIS) || (axis == Y_AXIS)) {
         abce_pos_t target = { planner.get_axis_position_mm(A_AXIS), planner.get_axis_position_mm(B_AXIS), planner.get_axis_position_mm(C_AXIS), planner.get_axis_position_mm(E_AXIS) };
         target[axis] = 0;
         planner.set_machine_position_mm(target);
         float dist = (distance > 0) ? -MOVE_BACK_BEFORE_HOMING_DISTANCE : MOVE_BACK_BEFORE_HOMING_DISTANCE;
         target[axis] = dist;
-    #if IS_KINEMATIC && DISABLED(CLASSIC_JERK)
-        const xyze_float_t delta_mm_cart { 0 };
-    #endif
         // Set delta/cartesian axes directly
-        planner.buffer_segment(target
-    #if IS_KINEMATIC && DISABLED(CLASSIC_JERK)
-            ,
-            delta_mm_cart
-    #endif
-            ,
-            real_fr_mm_s / 4, active_extruder);
+        planner.buffer_segment(target, real_fr_mm_s / 4, active_extruder);
         planner.synchronize();
     }
-#endif
 
     abce_pos_t target = { planner.get_axis_position_mm(A_AXIS), planner.get_axis_position_mm(B_AXIS), planner.get_axis_position_mm(C_AXIS), planner.get_axis_position_mm(E_AXIS) };
     target[axis] = 0;
     planner.set_machine_position_mm(target);
     target[axis] = distance;
 
-#if IS_KINEMATIC && DISABLED(CLASSIC_JERK)
-    const xyze_float_t delta_mm_cart { 0 };
-#endif
-
     // Set delta/cartesian axes directly
-    planner.buffer_segment(target
-#if IS_KINEMATIC && DISABLED(CLASSIC_JERK)
-        ,
-        delta_mm_cart
-#endif
-        ,
-        real_fr_mm_s, active_extruder);
-
+    planner.buffer_segment(target, real_fr_mm_s, active_extruder);
     planner.synchronize();
 
     if (is_home_dir) {
-
-#if HOMING_Z_WITH_PROBE && QUIET_PROBING
-        if (axis == Z_AXIS)
-            probing_pause(false);
-#endif
-
         endstops.validate_homing_move();
-
-// Re-enable stealthChop if used. Disable diag1 pin on driver.
-#if ENABLED(SENSORLESS_HOMING)
-        end_sensorless_homing_per_axis(axis, stealth_states);
-#endif
     }
 }
 
