6aff86db1c44785d61f15c1f9cb8bdc96ae57488
espr14 <espr14@gmail.com>
2021-01-26 18:24:23 +0100

    works

--- src/common/crash_recovery.cpp
+++ src/common/crash_recovery.cpp
@@ -9,38 +9,39 @@
 #include "../../Marlin/src/module/planner.h"
 #include "../../Marlin/src/module/stepper.h"
 
-void print_position(const int axis) {
+void print_position(const AxisEnum axis) {
     char text[10];
-    snprintf(text, 10, "%f", (double)current_position.pos[axis]);
-    display::DrawText(Rect16(0, 0, 100, 100), string_view_utf8::MakeRAM((const uint8_t *)text), GuiDefaults::Font, COLOR_BLACK, COLOR_WHITE);
+    snprintf(text, 10, "%f", (double)planner.get_axis_position_mm(axis));
+    display::DrawText(Rect16(0, 0, 100, 15), string_view_utf8::MakeRAM((const uint8_t *)text), GuiDefaults::Font, COLOR_BLACK, COLOR_WHITE);
 }
 
-void do_homing_move_crash(const AxisEnum axis, float distance, const feedRate_t fr_mm_s = 0.0) {
+void do_homing_move_crash(const AxisEnum axis, float distance) {
     if (!(axis == X_AXIS || axis == Y_AXIS))
         return;
 
     // if ((home_dir(axis) > 0) != (distance > 0))
     //     distance = -distance;
-    const feedRate_t real_fr_mm_s = fr_mm_s ?: homing_feedrate(axis);
+    const feedRate_t real_fr_mm_s = homing_feedrate(axis);
+    abce_pos_t target;
 
-    /// move back
-    abce_pos_t target = { planner.get_axis_position_mm(A_AXIS), planner.get_axis_position_mm(B_AXIS), planner.get_axis_position_mm(C_AXIS), planner.get_axis_position_mm(E_AXIS) };
-    target[axis] = 0;
-    planner.set_machine_position_mm(target);
-    target[axis] = (distance > 0) ? -MOVE_BACK_BEFORE_HOMING_DISTANCE : MOVE_BACK_BEFORE_HOMING_DISTANCE;
-    // Set delta/cartesian axes directly
-    planner.buffer_segment(target, real_fr_mm_s / 4, active_extruder);
-    planner.synchronize();
+    // /// move back
+    // abce_pos_t target = { planner.get_axis_position_mm(A_AXIS), planner.get_axis_position_mm(B_AXIS), planner.get_axis_position_mm(C_AXIS), planner.get_axis_position_mm(E_AXIS) };
+    // target[axis] = 0;
+    // planner.set_machine_position_mm(target);
+    // target[axis] = (distance > 0) ? -MOVE_BACK_BEFORE_HOMING_DISTANCE : MOVE_BACK_BEFORE_HOMING_DISTANCE;
+    // // Set delta/cartesian axes directly
+    // planner.buffer_segment(target, real_fr_mm_s / 4, active_extruder);
+    // planner.synchronize();
 
     /// home
     target = { planner.get_axis_position_mm(A_AXIS), planner.get_axis_position_mm(B_AXIS), planner.get_axis_position_mm(C_AXIS), planner.get_axis_position_mm(E_AXIS) };
-    target[axis] = 0;
-    planner.set_machine_position_mm(target);
-    target[axis] = distance;
+    // target[axis] = 0;
+    // planner.set_machine_position_mm(target);
+    target[axis] += distance;
 
-    // Set delta/cartesian axes directly
     planner.buffer_segment(target, real_fr_mm_s, active_extruder);
     planner.synchronize();
+    /// reset number of crashes
     endstops.validate_homing_move();
 }
 
@@ -54,6 +55,7 @@ void home_Marlin(const AxisEnum axis, int dir, bool reset_position = false) {
     // #define CAN_HOME_X true
     // #define CAN_HOME_Y true
     do_homing_move_crash(axis, 1.5f * max_length(axis) * dir);
+    planner.synchronize();
     print_position(axis);
 
     if (reset_position) {
@@ -119,17 +121,17 @@ void crash_recovery() {
     // home_to_start_Marlin(X_AXIS);
     home_Marlin(X_AXIS, 1, true);
 
-    const uint32_t m_StartPos_usteps = stepper.position((AxisEnum)X_AXIS);
+    // const uint32_t m_StartPos_usteps = stepper.position((AxisEnum)X_AXIS);
 
-    // home_Marlin(X_AXIS, -1);
+    home_Marlin(X_AXIS, -1);
 
     // display::FillRect(Rect16(0, 0, 10, 10), COLOR_WHITE);
 
     // marlin_print_resume();
 
-    const int32_t endPos_usteps = stepper.position((AxisEnum)X_AXIS);
-    const int32_t length_usteps = endPos_usteps - m_StartPos_usteps;
-    const float length_mm = (length_usteps * planner.steps_to_mm[(AxisEnum)X_AXIS]);
+    // const int32_t endPos_usteps = stepper.position((AxisEnum)X_AXIS);
+    // const int32_t length_usteps = endPos_usteps - m_StartPos_usteps;
+    // const float length_mm = (length_usteps * planner.steps_to_mm[(AxisEnum)X_AXIS]);
     // if (182 <= length_mm && length_mm <= 190) {
     //     display::FillRect(Rect16(0, 0, 10, 10), COLOR_LIME);
     // } else {
