2eddb405eb542fb1e5f253702f735a2f412b386e
espr14 <espr14@gmail.com>
2021-01-27 21:34:17 +0100

    Clean code

--- src/common/crash_recovery.cpp
+++ src/common/crash_recovery.cpp
@@ -44,8 +44,6 @@ void do_homing_move_crash(const AxisEnum axis, float distance) {
     if (!(axis == X_AXIS || axis == Y_AXIS))
         return;
 
-    // if ((home_dir(axis) > 0) != (distance > 0))
-    //     distance = -distance;
     const feedRate_t real_fr_mm_s = homing_feedrate(axis);
     abce_pos_t target;
 
@@ -60,8 +58,6 @@ void do_homing_move_crash(const AxisEnum axis, float distance) {
 
     /// home
     target = { planner.get_axis_position_mm(A_AXIS), planner.get_axis_position_mm(B_AXIS), planner.get_axis_position_mm(C_AXIS), planner.get_axis_position_mm(E_AXIS) };
-    // target[axis] = 0;
-    // planner.set_machine_position_mm(target);
     target[axis] += distance;
 
     planner.buffer_segment(target, real_fr_mm_s, active_extruder);
@@ -75,10 +71,7 @@ void home_Marlin(const AxisEnum axis, int dir, bool reset_position = false) {
         return;
 
     endstops.enable(true);
-    // #define CAN_HOME_X true
-    // #define CAN_HOME_Y true
     do_homing_move_crash(axis, 1.5f * max_length(axis) * dir);
-    planner.synchronize();
 
     if (reset_position) {
         set_axis_is_at_home(axis);
@@ -91,88 +84,24 @@ void home_Marlin(const AxisEnum axis, int dir, bool reset_position = false) {
             target[axis] = X_MAX_POS;
             planner.set_machine_position_mm(target);
         }
-
-        // destination[axis] = current_position[axis];
-        // current_position.pos[axis] = 0;
     }
 
     endstops.not_homing();
-    // line_to_current_position(100);
     planner.synchronize();
     // synchronize positions
     current_position.pos[axis] = planner.get_axis_position_mm(axis);
     sync_plan_position();
 }
 
-void home_to_start_Marlin(AxisEnum axis) {
-    endstops.enable(true);
-    homeaxis(axis);
-    endstops.not_homing();
-    current_position.pos[axis] = 0;
-    // line_to_current_position(100);
-    planner.synchronize();
-}
-
-void home_to_end_Marlin(AxisEnum axis) {
-
-    // stepperX.homing_threshold(X_STALL_SENSITIVITY);
-
-    endstops.enable(true);
-    homeaxis(axis);
-    endstops.not_homing();
-    current_position.pos[axis] = 0;
-    line_to_current_position(100);
-    planner.synchronize();
-}
-
-void home_to_end_custom(AxisEnum axis) {
-    endstops.enable(true);
-    // sg_sampling_enable();
-    current_position.pos[axis] = 190;
-    line_to_current_position(100);
-
-    if (planner.movesplanned())
-        gui_loop();
-
-    // sg_sampling_disable();
-    endstops.not_homing();
-}
-
 void crash_recovery() {
-    display::FillRect(Rect16(0, 0, 10, 10), COLOR_GREEN);
     marlin_print_pause();
     while (marlin_vars()->print_state != mpsPaused)
         gui_loop();
 
-    // print_all(X_AXIS);
-
-    // home_to_start_Marlin(X_AXIS);
     home_Marlin(X_AXIS, 1, true);
-    // print_all(X_AXIS);
-
-    // const uint32_t m_StartPos_usteps = stepper.position((AxisEnum)X_AXIS);
     float length = planner.get_axis_position_mm(X_AXIS);
-
-    // current_position.pos[X_AXIS] = 10;
-    // line_to_current_position(100);
-    // planner.synchronize();
-
     home_Marlin(X_AXIS, -1);
-    // print_all(X_AXIS);
-
-    // char text[10];
-    // snprintf(text, 10, "%f", (double)planner.get_axis_position_mm(X_AXIS));
-    // display::DrawText(Rect16(0, 0, 100, 15), string_view_utf8::MakeRAM((const uint8_t *)text), GuiDefaults::Font, COLOR_BLACK, COLOR_WHITE);
-
     length -= planner.get_axis_position_mm(X_AXIS);
-    // char text[10];
-    // snprintf(text, 10, "%f", (double)length);
-    // display::DrawText(Rect16(0, 0, 100, 15), string_view_utf8::MakeRAM((const uint8_t *)text), GuiDefaults::Font, COLOR_BLACK, COLOR_WHITE);
-
     if (182 <= length && length <= 190)
         marlin_print_resume();
-
-    // const int32_t endPos_usteps = stepper.position((AxisEnum)X_AXIS);
-    // const int32_t length_usteps = endPos_usteps - m_StartPos_usteps;
-    // const float length_mm = (length_usteps * planner.steps_to_mm[(AxisEnum)X_AXIS]);
 }
